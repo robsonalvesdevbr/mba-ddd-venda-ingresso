97a30518631c462ba9816f84d61d03933bac1482
Robson Alves <contato@robsonalves.dev.br>
2025-01-05 14:51:07 -0300

    refactor Customer entity to use CustomerId value object and update tests accordingly

--- src/@core/events/domain/entities/__tests__/customer.vo.spec.ts
+++ src/@core/events/domain/entities/__tests__/customer.vo.spec.ts
@@ -1,8 +1,13 @@
-import { Customer } from '../customer.entity';
+import { Customer, CustomerId } from '../customer.entity';
 
 test('deve criar um cliente', () => {
-  Customer.create({
+  const customer = Customer.create({
     name: 'John Doe',
     cpf: '03333207940',
   });
+  expect(customer).toBeInstanceOf(Customer);
+  expect(customer.id).toBeDefined();
+  expect(customer.id).toBeInstanceOf(CustomerId);
+  expect(customer.name).toBe('John Doe');
+  expect(customer.cpf.value).toBe('03333207940');
 });
